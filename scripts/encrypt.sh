78273385512b7aaf85dc1375235352048b833184
dr460nf1r3 <njcrypted@protonmail.com>
2022-05-04 20:22:29 +0200

    Fix typo & prettify

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-for host in $(ls ./ansible/host_vars); do 
-    cd ansible && ansible-vault encrypt host_vars/"$host"/"$host"_vault.yml &> /dev/null || true
-    cd ../
+for host in $(ls ./ansible/host_vars); do
+  cd ansible && ansible-vault encrypt host_vars/"$host"/"$host"_vault.yml &>/dev/null || true
+  cd ../
 done
 
 echo "[38;5;208mEncrypted all vaults![0m"
--- scripts/vault_checker.sh
+++ scripts/vault_checker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-for host in $(ls ./ansible/host_vars); do 
-    if test -f ansible/host_vars/"$host"/"$host"_vault.yml; then
-        if ( grep -q "\$ANSIBLE_VAULT;" < ansible/host_vars/"$host"/"$host"_vault.yml ); then
-            echo "[38;5;108mVault Encrypted. Safe to commit.[0m"
-        else
-            echo "[38;5;208mCleartext vault detected! Run ./scripts/encrypt.sh and try again.[0m"
-            exit 1
-        fi
+for host in $(ls ./ansible/host_vars); do
+  if test -f ansible/host_vars/"$host"/"$host"_vault.yml; then
+    if (grep -q "\$ANSIBLE_VAULT;" <ansible/host_vars/"$host"/"$host"_vault.yml); then
+      echo "[38;5;108mVault Encrypted. Safe to commit.[0m"
+    else
+      echo "[38;5;208mCleartext vault detected! Run ./scripts/encrypt.sh and try again.[0m"
+      exit 1
     fi
+  fi
 done
